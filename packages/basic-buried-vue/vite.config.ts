86924e520260ab0c89b570564aae8234ad8ee0dd
chenxueli <zjphchenxueli@163.com>
2023-05-19 10:59:38 +0800

    修改buried的vue打包

---
+++ packages/basic-buried-vue/vite.config.ts
@@ -0,0 +1,42 @@
+/// <reference types="vitest" />
+
+import { resolve } from 'node:path'
+import Vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import ViteDts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    Vue({
+      reactivityTransform: true,
+    }),
+    ViteDts({
+      skipDiagnostics: true,
+    }),
+  ],
+  // https://github.com/vitest-dev/vitest
+  test: {
+    environment: 'jsdom',
+  },
+  build: {
+    target: 'modules',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs', 'umd'],
+      name: 'BasicUiNaive',
+      // the proper extensions will be added
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
