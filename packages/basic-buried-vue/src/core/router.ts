d014cfea719caec1dff6c7658d7db6946ad91524
chenxueli <zjphchenxueli@163.com>
2023-05-21 12:49:11 +0800

    优化vue的埋点数据推送

---
+++ packages/basic-buried-vue/src/core/router.ts
@@ -0,0 +1,38 @@
+import { sendPv, sendSt } from '@own-basic-component/buried'
+import type { createRouter } from 'vue-router'
+
+/**
+ * 初始化路由数据
+ * @param router
+ */
+export default function setRouterBuried(router: ReturnType<typeof createRouter>) {
+  // 记录开始时间
+  let startTimestamp = Date.now()
+
+  router.beforeEach((to, from, next) => {
+    // 发送停留时间
+    const d = Date.now() - startTimestamp
+    // 如果时间大于2s才会发送
+    if (d > 2000) {
+      sendSt({
+        // 来源地址
+        fromPagePath: from.path || '',
+        // 来源名称
+        fromPageName: from.name?.toString() || '',
+        // 目的地址
+        toPagePath: to.path || '',
+        // 目的名称
+        toPageName: to.name?.toString() || '',
+        // 停留时间
+        d: `${d}`,
+      })
+    }
+    next()
+  })
+
+  // 跳转页面的时候发送访问的pv
+  router.afterEach(() => {
+    sendPv()
+    startTimestamp = Date.now()
+  })
+}
--- packages/basic-buried-vue/src/index.ts
+++ packages/basic-buried-vue/src/index.ts
@@ -1,8 +1,12 @@
 'use strict'
 
 // 按钮埋点的指令事件
-import buriedDirective from './directive/buried'
+import buriedDirective from './core/directive-buried'
+
+// 路由的埋点
+import setRouterBuried from './core/router'
 
 export {
   buriedDirective,
+  setRouterBuried,
 }
