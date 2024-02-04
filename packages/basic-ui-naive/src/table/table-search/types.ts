1d65c1826ca7591068765cedf194904c92903a19
chenxueli <zjphchenxueli@163.com>
2024-02-04 14:47:45 +0800

    添加更多的操作

--- packages/basic-ui-naive/src/table/table-search/types.ts
+++ packages/basic-ui-naive/src/table/table-search/types.ts
@@ -41,9 +41,19 @@ export interface SearchPropsType {
 export type RequiredField<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
 /**
+ * 将某个字段改为选填
+ */
+export type PartialField<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+/**
  * 将多个字段改为必填
  */
 export type RequiredFields<T, K extends (keyof T)[]> = Omit<T, K[number]> & Required<Pick<T, K[number]>>
+
+/**
+ * 将多个字段改为选填
+ */
+export type PartialFields<T, K extends (keyof T)[]> = Omit<T, K[number]> & Partial<Pick<T, K[number]>>
 
 /**
  * 类型key信息
