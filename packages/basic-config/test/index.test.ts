c78808d1f3003ea5ec49c485634f59a7102ea55c
chenxueli <zjphchenxueli@163.com>
2024-04-17 09:27:47 +0800

    测试

--- packages/basic-config/test/index.test.ts
+++ packages/basic-config/test/index.test.ts
@@ -1,7 +1,45 @@
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
+import { defineProjectConfig, getProjectConfig, getRequestConfig } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+it('should work', () => {
+  defineProjectConfig({
+    baseUrl: '/api',
+    request: {
+      success: ['00000', '0'],
+    },
   })
+  const config = getProjectConfig()
+  expect(config).toMatchInlineSnapshot(`
+    {
+      "baseUrl": "/api",
+      "request": {
+        "success": [
+          "00000",
+          "0",
+        ],
+      },
+    }
+  `)
+  const requestConfig = getRequestConfig()
+  expect(requestConfig).toMatchInlineSnapshot(`
+    {
+      "baseUrl": "/api",
+      "contentType": "application/json;charset=UTF-8",
+      "field": {
+        "code": "code",
+        "data": "data",
+        "message": "message",
+      },
+      "header": [Function],
+      "mask": false,
+      "mergeType": "replace",
+      "paramType": "text",
+      "preprocess": [Function],
+      "success": [
+        "00000",
+        "0",
+      ],
+      "timeout": 10000,
+    }
+  `)
 })
