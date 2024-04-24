495975d9932cda7f3f0399e9abadec58dba359ff
chenxueli <zjphchenxueli@163.com>
2024-04-24 15:01:06 +0800

    内容选择参数优化

--- packages/basic-vue/src/component/BaseFileSelectButton.vue
+++ packages/basic-vue/src/component/BaseFileSelectButton.vue
@@ -7,10 +7,25 @@ import { resolveFileList, resolveFileListToArray } from '../composables'
  * 定义传入参数
  */
 const props = withDefaults(defineProps<{
+  /**
+   * 显示的文字
+   */
   text?: string
+  /**
+   * 图片时间
+   */
   resolveFileMethod?: FileResolveMethod<T>
+  /**
+   * 是否支持多选
+   */
+  multiple?: boolean
+  /**
+   * 接受的文件类型
+   */
+  accept?: string
 }>(), {
   text: '选择文件',
+  multiple: false,
 })
 
 /**
@@ -63,6 +78,13 @@ async function handleChangeSelectFile(e: Event) {
     <div v-if="slots.default" @click="handleSelectFile">
       <slot />
     </div>
-    <input ref="fileRef" multiple hidden type="file" @change="handleChangeSelectFile">
+    <input
+      ref="fileRef"
+      :multiple="props.multiple"
+      :accept="props.accept"
+      hidden
+      type="file"
+      @change="handleChangeSelectFile"
+    >
   </div>
 </template>
